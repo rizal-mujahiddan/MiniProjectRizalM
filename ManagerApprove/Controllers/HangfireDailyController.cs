b464f7a2fe119b88def16211dd0fbac57f534596
rizal-mujahiddan <rizal.mujahiddan@gmail.com>
2024-06-29 10:47:05 +0700

    Berhasil Hangfire

--- ManagerApprove/Controllers/HangfireDailyController.cs
+++ ManagerApprove/Controllers/HangfireDailyController.cs
@@ -7,17 +7,9 @@ namespace ManagerApprove.Controllers
 {
     public class HangfireDailyController : Controller
     {
-        private readonly MiniProjDbContext _context;
-        private readonly HangfireService _hangfireService;
-        public HangfireDailyController(MiniProjDbContext context, HangfireService hangfireService)
-        {
-            _context = context;
-            _hangfireService = hangfireService;
-        }
         public IActionResult Index()
         {
-            RecurringJob.AddOrUpdate(() => _hangfireService.Print());
-
+            RecurringJob.AddOrUpdate((HangfireService hs) => hs.Print(),Cron.Minutely);
             return Redirect("/Hangfire");
             //return Json();
         }
